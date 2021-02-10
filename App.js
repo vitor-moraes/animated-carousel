bbe3990fa54ec4c2dbb0f489b716245679e99609
vitor-moraes <vitormoraes3@yahoo.com.br>
2021-02-10 16:25:31 -0300

    Feat: ended

--- App.js
+++ App.js
@@ -1,21 +1,193 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect, useRef } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Dimensions,
+  Image,
+  Animated,
+} from "react-native";
+const { width, height } = Dimensions.get("window");
+import { getMovies } from "./src/services/api";
+import Genres from "./src/components/Genres";
+import Rating from "./src/components/Rating";
+import MaskedView from '@react-native-community/masked-view';
+import Svg, { Rect } from 'react-native-svg';
+import { LinearGradient } from 'expo-linear-gradient';
+
+const SPACING = 10;
+const ITEM_SIZE = width * 0.72;
+const EMPTY_ITEM_SIZE = (width - ITEM_SIZE) / 2;
+const BACKDROP_HEIGHT = height * 0.65;
+const AnimatedSvg = Animated.createAnimatedComponent(Svg);
+
+const Loading = () => {
+  return (
+    <View style={styles.loadingContainer}>
+      <Text style={styles.paragraph}>Loading...</Text>
+    </View>
+  );
+};
+
+const Backdrop = ({movies, scrollX}) => {
+  return <View style={{ height: BACKDROP_HEIGHT, width, position: 'absolute' }}>
+        <FlatList
+          data={movies.reverse()}
+          keyExtractor={(item) => item.key + '-backdrop'}
+          removeClippedSubviews={false}
+          contentContainerStyle={{width, height: BACKDROP_HEIGHT}}
+          renderItem={({ item, index }) => {
+            if (!item.backdrop) {
+              return null;
+            }
+            const translateX = scrollX.interpolate({
+              inputRange: [(index - 2) * ITEM_SIZE, (index - 1) * ITEM_SIZE],
+              outputRange: [0, width],
+              // extrapolate:'clamp'
+            });
+            return (
+              <Animated.View
+                removeClippedSubviews={false}
+                style={{position: 'absolute', width: translateX, height, overflow: 'hidden'}}
+              >
+              <Image
+                source={{ uri: item.backdrop }}
+                style={{
+                  width,
+                  height: BACKDROP_HEIGHT,
+                  position: 'absolute'
+                }}
+              />
+              </Animated.View>
+            );
+          }}
+        />
+        <LinearGradient
+          colors={['rgba(0, 0, 0, 0)', 'white']}
+          style={{
+            height: BACKDROP_HEIGHT,
+            width,
+            position: 'absolute',
+            bottom: 0,
+          }}
+        />
+      </View>
+}
 
 export default function App() {
+  const [movies, setMovies] = useState([]);
+  const scrollX = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const movies = await getMovies();
+      // [spacer, .. movies, spacer]
+      setMovies([{ key: "left-spacer" }, ...movies, { key: "right-spacer" }]);
+    };
+
+    if (movies.length === 0) {
+      fetchData(movies);
+    }
+  }, [movies]);
+
+  if (movies.length === 0) {
+    return <Loading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Backdrop movies={movies} scrollX={scrollX} />
+      <StatusBar hidden />
+      <Animated.FlatList
+        showsHorizontalScrollIndicator={false}
+        data={movies}
+        keyExtractor={(item) => item.key}
+        horizontal
+        bounces={false}
+        decelerationRate={Platform.OS === 'ios' ? 0 : 0.98}
+        
+        renderToHardwareTextureAndroid
+        contentContainerStyle={{ alignItems: 'center' }}
+        snapToInterval={ITEM_SIZE}
+        snapToAlignment='start'
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: false }
+        )}
+        scrollEventThrottle={16}
+        renderItem={({ item, index }) => {
+          if (!item.poster) {
+            return <View style={{ width: EMPTY_ITEM_SIZE }} />;
+          }
+
+          const inputRange = [
+            (index - 2) * ITEM_SIZE,
+            (index - 1) * ITEM_SIZE,
+            index * ITEM_SIZE,
+          ];
+
+          const translateY = scrollX.interpolate({
+            inputRange,
+            outputRange: [100, 50, 100],
+            extrapolate: 'clamp'
+          });
+
+          return (
+            <View style={{ width: ITEM_SIZE }}>
+              <Animated.View
+                style={{
+                  marginHorizontal: SPACING,
+                  padding: SPACING * 2,
+                  alignItems: 'center',
+                  transform: [{ translateY }],
+                  backgroundColor: 'white',
+                  borderRadius: 34,
+                }}
+              >
+                <Image
+                  source={{ uri: item.poster }}
+                  style={styles.posterImage}
+                />
+                <Text style={{ fontSize: 24 }} numberOfLines={1}>
+                  {item.title}
+                </Text>
+                <Rating rating={item.rating} />
+                <Genres genres={item.genres} />
+                <Text style={{ fontSize: 12 }} numberOfLines={3}>
+                  {item.description}
+                </Text>
+              </Animated.View>
+            </View>
+          );
+        }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  posterImage: {
+    width: "100%",
+    height: ITEM_SIZE * 1.2,
+    resizeMode: "cover",
+    borderRadius: 24,
+    margin: 0,
+    marginBottom: 10,
   },
 });
